630ba43a63777a6132bb75f1e73273d66c33bbb7
CarlosMoralesChala <100883913+Chala14@users.noreply.github.com>
2022-05-03 20:41:21 -0500

    Cambios

---
+++ MesadeAyuda/TicketArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesadeAyuda
+{
+    public class TicketArchivo
+    {
+        public Ticket[] Ticket;
+        void GenerarTXT()
+        {
+            string rutaCompleta = @"Tickets.csv";
+            string texto = string.Join(",", Ticket.Select(n => n.ToString()).ToArray());
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                DateTime dateTime = new DateTime();
+                dateTime = DateTime.Now;
+                string strDate = Convert.ToDateTime(dateTime).ToString("yyMMdd");
+                mylogs.WriteLine(texto + strDate);
+                mylogs.Close();
+            }
+        }
+    }
+
+}
