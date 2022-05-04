e857af94f5f228416686296f566d90d26202c066
CarlosMoralesChala <100883913+Chala14@users.noreply.github.com>
2022-05-03 20:43:35 -0500

    Cambios

---
+++ MesadeAyuda/SubCategoriaArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesadeAyuda
+{
+    public class SubcategoriaArchivo
+    {
+        public Subcategoria[] Subcategoria;
+        void GenerarTXT()
+        {
+            string rutaCompleta = @"Subcategorias.csv";
+            string texto = string.Join(",", Subcategoria.Select(n => n.ToString()).ToArray());
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                DateTime dateTime = new DateTime();
+                dateTime = DateTime.Now;
+                string strDate = Convert.ToDateTime(dateTime).ToString("yyMMdd");
+                mylogs.WriteLine(texto + strDate);
+                mylogs.Close();
+            }
+        }
+    }
+
+}
