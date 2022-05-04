f4411b32331b14823afce1a9a367a25e03ce7dcb
CarlosMoralesChala <100883913+Chala14@users.noreply.github.com>
2022-05-03 20:42:21 -0500

    Cambios

---
+++ MesadeAyuda/GrupoSoporteArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesadeAyuda
+{
+    public class GrupoSoporteArchivo
+    {
+        public GrupoSoporte[] GrupoSoporte;
+        void GenerarTXT()
+        {
+            string rutaCompleta = @"GruposSoporte.csv";
+            string texto = string.Join(",", GrupoSoporte.Select(n => n.ToString()).ToArray());
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                DateTime dateTime = new DateTime();
+                dateTime = DateTime.Now;
+                string strDate = Convert.ToDateTime(dateTime).ToString("yyMMdd");
+                mylogs.WriteLine(texto + strDate);
+                mylogs.Close();
+            }
+        }
+    }
+
+}
