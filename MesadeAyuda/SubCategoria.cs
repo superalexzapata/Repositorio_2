95428a3fd6ae0ffaab50688c33282f3b12faa1ec
CarlosMoralesChala <100883913+Chala14@users.noreply.github.com>
2022-05-03 20:38:30 -0500

    Cambios

--- MesadeAyuda/SubCategoria.cs
+++ MesadeAyuda/SubCategoria.cs
@@ -9,6 +9,25 @@ namespace MesadeAyuda
     public class SubCategoria
     {
 
-        
+        private int IdSubCategoria;
+        private string NombreSubCategoria;
+        public Categoria Categoria;
+
+        public SubCategoria(Categoria categoria)/*Composición*/
+        {
+            this.Categoria = categoria;
+        }
+        public void CrearSubCategoria()/*Método*/
+        {
+        }
+
+        public void ConsultarSubCategoria()/*Método*/
+        {
+        }
+
+        public void ActualizarSubCategoria()/*Método*/
+        {
+        }
     }
+
 }
