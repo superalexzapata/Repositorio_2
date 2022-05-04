c1683c94e7f16a7ad5463217974204a6eb99ac38
CarlosMoralesChala <100883913+Chala14@users.noreply.github.com>
2022-05-03 20:39:20 -0500

    Cambios

--- MesadeAyuda/Estadistica.cs
+++ MesadeAyuda/Estadistica.cs
@@ -8,6 +8,26 @@ namespace MesadeAyuda
 {
     public class Estadistica
     {
-        
+        private int Id;
+        public string NombreEstadistica;
+        public Array Ticket;
+        public Prioridad Prioridad;
+
+        public Estadistica(Prioridad prioridad)/*Composición*/
+        {
+            this.Prioridad = prioridad;
+        }
+        public void CrearEstadistica()/*Método*/
+        {
+        }
+
+        public void ConsultarEstadistica()/*Método*/
+        {
+        }
+
+        public void ActualizarPrioridad()/*Método*/
+        {
+        }
     }
+
 }
