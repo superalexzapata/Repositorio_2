25168814e95e574a94fa6338b3875a7c31c236c4
chala14 <cjmorales93543@umanizales.edu.co>
2022-05-25 21:05:31 -0500

    Actualización Prioridad : Baja, Media, Alta

--- MesadeAyuda/Prioridad.cs
+++ MesadeAyuda/Prioridad.cs
@@ -3,8 +3,14 @@
     public class Prioridad
     {
         private int Id;
+        public enum TipoPrioridad
+        {
+            Baja = 1,
+            Media = 2,
+            Alta = 3,
+            
+        }
         private string NombrePrioridad;
-
 
         public void CrearPrioridad()/*Método*/
         {
