8ae1ad90830ef1aaa254be667ffb879a7a87ebbf
chala14 <cjmorales93543@umanizales.edu.co>
2022-05-27 03:48:52 -0500

    Actualizar Bintácora Archivo

---
+++ MesadeAyuda/BitacoraArchivo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace MesadeAyuda
+{
+    internal class BitacoraArchivo
+    {
+        public List<Bitacora> listaBitacoras;
+        public void GenerarTxtBitacoras()
+        {
+            string rutaCompleta = @"7Bitacoras.csv";
+
+            var unaCadenadeBitacoras = new StringBuilder();
+
+            // Build the users string.
+            foreach (Bitacora unBitac in listaBitacoras)
+            {
+                //unaCadenadeBitacoras.Append(unBitac.Id);
+                //unaCadenadeBitacoras.Append(";");
+                unaCadenadeBitacoras.Append(unBitac.NombreBitacora);
+                unaCadenadeBitacoras.Append(";");
+                unaCadenadeBitacoras.Append("\n");
+
+            }
+
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                mylogs.WriteLine(unaCadenadeBitacoras.ToString());
+                mylogs.Close();
+            }
+        }
+    }
+}
