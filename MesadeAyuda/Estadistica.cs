3ec1c0902654c746a26c02bebd36e0276d0cc957
chala14 <cjmorales93543@umanizales.edu.co>
2022-05-27 02:51:56 -0500

    Actualización Estadistica

--- MesadeAyuda/Estadistica.cs
+++ MesadeAyuda/Estadistica.cs
@@ -2,7 +2,23 @@
 {
     public class Estadistica
     {
-        
+        private int Id;
+        public string NombreEstadistica;
+
+
+
+        public void CrearEstadistica(int estad)/*Método*/
+        {
+            Id = estad;
+        }
+
+        public void ConsultarEstadistica()/*Método*/
+        {
+        }
+
+        public void ActualizarPrioridad()/*Método*/
+        {
+        }
     }
 
 }
