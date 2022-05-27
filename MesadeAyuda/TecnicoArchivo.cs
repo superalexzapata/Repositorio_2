b9a76917fba01fd9f77a16807668f1c309061d0d
chala14 <cjmorales93543@umanizales.edu.co>
2022-05-26 23:31:03 -0500

    Actualizar Tecnico Archivo

--- MesadeAyuda/TecnicoArchivo.cs
+++ MesadeAyuda/TecnicoArchivo.cs
@@ -1,20 +1,45 @@
 ﻿namespace MesadeAyuda
 {
-    public class TecnicoArchivo
+    public List<Tecnico> listaTecnicos;
+    public void GenerarTxtTecnicos()
     {
-        public Tecnico[] Tecnico;
-        void GenerarTXT()
+        string rutaCompleta = @"Tecnicos.csv";
+
+        var unaCadenadeTecnicos = new StringBuilder();
+
+        // Build the users string.
+        foreach (Tecnico unTecnico in listaTecnicos)
         {
-            string rutaCompleta = @"Tecnicos.csv";
-            string texto = string.Join(",", Tecnico.Select(n => n.ToString()).ToArray());
-            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
-            {
-                DateTime dateTime = new DateTime();
-                dateTime = DateTime.Now;
-                string strDate = Convert.ToDateTime(dateTime).ToString("yyMMdd");
-                mylogs.WriteLine(texto + strDate);
-                mylogs.Close();
-            }
+            //unaCadenadeTecnicos.Append(unUsuario.tipoDoc);
+            //unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.NumeroIdentificacion);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Nombre);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Direccion);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Edad);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.EsActivo);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Telefono);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Email);
+            unaCadenadeTecnicos.Append(";");
+            //unaCadenadeTecnicos.Append(unTecnico.Sexo);
+            //unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.Password);
+            unaCadenadeTecnicos.Append(";");
+            unaCadenadeTecnicos.Append(unTecnico.GrupoSoporte.GrupoSop);
+            unaCadenadeTecnicos.Append("\n");
+
+        }
+
+        using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+        {
+            mylogs.WriteLine(unaCadenadeTecnicos.ToString());
+            mylogs.Close();
         }
     }
+}
 }
