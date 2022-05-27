dca7932e61c4c2b6fe3095e9f0f6e5cc4609b296
chala14 <cjmorales93543@umanizales.edu.co>
2022-05-27 02:20:21 -0500

    Actualizar Tipo Solicitud Archivo

--- MesadeAyuda/TipoSolicitudArchivo.cs
+++ MesadeAyuda/TipoSolicitudArchivo.cs
@@ -5,6 +5,29 @@ namespace MesadeAyuda
 {
     public class TipoSolicitudArchivo
     {
-        
+        public List<TipoSolicitud> listaTipoSol;
+        public void GenerarTxtTTiposol()
+        {
+            string rutaCompleta = @"Tipo_Solicitudes.csv";
+
+            var unaCadenadeTipos = new StringBuilder();
+
+            // Build the users string.
+            foreach (TipoSolicitud unTipo in listaTipoSol)
+            {
+                //unaCadenadeTipos.Append(unTipo.Id);
+                //unaCadenadeTipos.Append(";");
+                unaCadenadeTipos.Append(unTipo.NombreSolicitud);
+                unaCadenadeTipos.Append(";");
+                unaCadenadeTipos.Append("\n");
+
+            }
+
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                mylogs.WriteLine(unaCadenadeTipos.ToString());
+                mylogs.Close();
+            }
+        }
     }
 }
