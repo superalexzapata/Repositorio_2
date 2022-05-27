fa8baf1aac9638d917f14da617f39d708cdcc977
superalexzapata <alexzapata0115@gmail.com>
2022-05-27 00:42:25 -0500

    Usuario archivo

--- MesadeAyuda/UsuarioArchivo.cs
+++ MesadeAyuda/UsuarioArchivo.cs
@@ -5,6 +5,51 @@ namespace MesadeAyuda
 {
     public class UsuarioArchivo
     {
-        
+        public List<Usuario> listaUsuarios;
+        public void GenerarTxtUsuarios()
+        {
+            string rutaCompleta = @"Usuarios.csv";
+
+            var unaCadenadeUsuarios = new StringBuilder();
+
+            // Build the users string.
+            foreach (Usuario unUsuario in listaUsuarios)
+            {
+                //unaCadenadeUsuarios.Append(unUsuario.id);
+                //unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.TipoIdentificacion);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.NumeroIdentificacion);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Nombre);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Direccion);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Edad);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.EsActivo);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Telefono);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Email);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Sexo);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.Password);
+                unaCadenadeUsuarios.Append(";");
+                unaCadenadeUsuarios.Append(unUsuario.UbicacionUsuario.NombreUbicacion);
+                unaCadenadeUsuarios.Append("\n");
+
+            }
+
+            using (StreamWriter mylogs = File.AppendText(rutaCompleta))
+            {
+                mylogs.WriteLine(unaCadenadeUsuarios.ToString());
+                mylogs.Close();
+            }
+
+
+
+        }
     }
 }
